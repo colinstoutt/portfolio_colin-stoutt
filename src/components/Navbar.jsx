cb40a764f292f21a72f1a1169aaf415434f20a40
cps97 <colinstoutt@gmail.com>
2023-03-17 23:42:25 -0700

    complete index page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,28 @@
+import { Visibility } from "@mui/icons-material";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import LightModeIcon from "@mui/icons-material/LightMode";
 import { color } from "@mui/system";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useState } from "react";
+import { render } from "react-dom";
 
 const Nav = () => {
   const router = useRouter();
+  const [lightMode, setLightMode] = useState(false);
+
   return (
     <>
       <div className="nav">
-        <Link className="nav__name-link" href="/">
-          <span className="nav__name">Colin Stoutt</span>
+        <Link className="nav__name-link fade-in-image" href="/">
+          <span className="nav__name">CS</span>
         </Link>
         <div>
           <Link
             href="/about"
-            className="nav__link"
+            className="nav__link fade-in-image"
             style={
               router.route === "/about"
                 ? { color: "white" }
@@ -26,7 +33,7 @@ const Nav = () => {
           </Link>
           <Link
             href="/projects"
-            className="nav__link"
+            className="nav__link fade-in-image"
             style={
               router.route === "/projects"
                 ? { color: "white" }
@@ -37,7 +44,7 @@ const Nav = () => {
           </Link>
           <Link
             href="/contact"
-            className="nav__link"
+            className="nav__link fade-in-image"
             style={
               router.route === "/contact"
                 ? { color: "white" }
@@ -48,8 +55,14 @@ const Nav = () => {
           </Link>
         </div>
       </div>
-      <div className="nav-social-links">
-        <div className="nav-social-links__line"></div>
+      <div className="nav-social-links fade-in-image">
+        <div className="nav__lightmode fade-in-image">
+          <LightModeIcon
+            onClick={() => setLightMode(!lightMode)}
+            style={lightMode ? { color: "black" } : { color: "grey" }}
+          />
+        </div>
+        <div className="nav-social-links__line fade-in-image"></div>
         <a className="nav__social-link" href="https://github.com/colinstoutt">
           <GitHubIcon sx={{ fontSize: "2.5rem" }} />
         </a>
@@ -59,7 +72,7 @@ const Nav = () => {
         >
           <LinkedInIcon sx={{ fontSize: "2.5rem", marginLeft: "0.5rem" }} />
         </a>
-        <div className="nav-social-links__line"></div>
+        <div className="nav-social-links__line fade-in-image"></div>
       </div>
     </>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,8 @@ export default function Index() {
   return (
     <>
       <main className="index">
-        <h1 className="index__name">colinStoutt</h1>
-        <h1 className="index__title">webDeveloper</h1>
+        <h1 className="index__name fade-in-image">colinStoutt</h1>
+        <h1 className="index__title fade-in-image">webDeveloper</h1>
       </main>
     </>
   );
