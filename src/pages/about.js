6bd2451a273d1c777e659d5f1824f7248b15886c
cps97 <colinstoutt@gmail.com>
2023-03-19 00:27:17 -0700

    create style sheets and create about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,105 @@
 import React from "react";
 
 const about = () => {
-  return <div>about</div>;
+  return (
+    <main className="about">
+      <section className="about__section-about">
+        <h1 className="heading">A little about me.</h1>
+        <p>
+          My name is Colin Stoutt, I’m a web developer with a creative edge and
+          a strong background in customer service. I recently graduated from
+          General Assemblys Software Engineering Immersive.
+        </p>
+      </section>
+      <section className="about__section-skills">
+        <h1 className="heading">My skills.</h1>
+        <span
+          className="about__section-skills-skill"
+          style={{ backgroundColor: "#ea6228" }}
+        >
+          HTML
+        </span>
+        <span
+          className="about__section-skills-skill"
+          style={{ backgroundColor: "#2762eb" }}
+        >
+          CSS
+        </span>
+        <span
+          className="about__section-skills-skill"
+          style={{ backgroundColor: "#c86396" }}
+        >
+          SCSS
+        </span>
+        <span
+          className="about__section-skills-skill"
+          style={{ backgroundColor: "#3b78a8" }}
+        >
+          Python
+        </span>
+        <span
+          className="about__section-skills-skill"
+          style={{ backgroundColor: "#e9d54d" }}
+        >
+          Javascript
+        </span>
+        <span
+          className="about__section-skills-skill"
+          style={{ backgroundColor: "#2f75c0 " }}
+        >
+          Typescript
+        </span>
+        <span
+          className="about__section-skills-skill"
+          style={{ backgroundColor: "lightgrey" }}
+        >
+          NextJS
+        </span>
+        <span
+          className="about__section-skills-skill"
+          style={{ backgroundColor: "lightgrey" }}
+        >
+          Express
+        </span>
+        <span
+          className="about__section-skills-skill"
+          style={{ backgroundColor: "#46a93a" }}
+        >
+          MongoDB
+        </span>
+        <span
+          className="about__section-skills-skill"
+          style={{ backgroundColor: "#ac4343" }}
+        >
+          Mongoose
+        </span>
+        <span
+          className="about__section-skills-skill"
+          style={{ backgroundColor: "#31638d " }}
+        >
+          PostgreSQL
+        </span>
+        <span
+          className="about__section-skills-skill"
+          style={{ backgroundColor: "#2aa474" }}
+        >
+          Django
+        </span>
+        <span
+          className="about__section-skills-skill"
+          style={{ backgroundColor: "lightgrey" }}
+        >
+          Git Version Control
+        </span>
+        <span
+          className="about__section-skills-skill"
+          style={{ backgroundColor: "lightgrey" }}
+        >
+          Github
+        </span>
+      </section>
+    </main>
+  );
 };
 
 export default about;
