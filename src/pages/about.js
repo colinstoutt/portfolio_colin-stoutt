e2e124e5554ddc8a0d9ed46fadc5290f6898ca14
cps97 <colinstoutt@gmail.com>
2023-03-20 15:08:04 -0700

    remove color borders, make skills smaller, adjust spacing

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,100 +2,102 @@ import React from "react";
 
 const about = () => {
   return (
-    <main className="about">
+    <main className="about container">
       <section className="about__section-about">
-        <h1 className="heading">A little about me.</h1>
+        <h1 className="heading">
+          A little about me<div className="line"></div>
+        </h1>
         <p>
-          My name is Colin Stoutt, I’m a web developer with a creative edge and
-          a strong background in customer service. I recently graduated from
+          My name is Colin Stoutt, I’m a software developer with a creative edge
+          and a strong background in customer service. I recently graduated from
           General Assemblys Software Engineering Immersive.
         </p>
       </section>
       <section className="about__section-skills">
-        <h1 className="heading">My skills.</h1>
+        <h1 className="about__section-skills-heading">Technology</h1>
 
         <div className="about__section-skills-container">
           <span
             className="about__section-skills-skill"
-            style={{ border: "1px solid #ea6228" }}
+            // style={{ border: "1px solid #ea6228" }}
           >
             HTML
           </span>
           <span
             className="about__section-skills-skill"
-            style={{ border: "1px solid #2762eb" }}
+            // style={{ border: "1px solid #2762eb" }}
           >
             CSS
           </span>
           <span
             className="about__section-skills-skill"
-            style={{ border: "1px solid #c86396" }}
+            // style={{ border: "1px solid #c86396" }}
           >
             SCSS
           </span>
           <span
             className="about__section-skills-skill"
-            style={{ border: "1px solid #3b78a8" }}
+            // style={{ border: "1px solid #3b78a8" }}
           >
             Python
           </span>
           <span
             className="about__section-skills-skill"
-            style={{ border: "1px solid #e9d54d" }}
+            // style={{ border: "1px solid #e9d54d" }}
           >
             Javascript
           </span>
           <span
             className="about__section-skills-skill"
-            style={{ border: "1px solid #2f75c0 " }}
+            // style={{ border: "1px solid #2f75c0 " }}
           >
             Typescript
           </span>
           <span
             className="about__section-skills-skill"
-            style={{ border: "1px solid lightgrey" }}
+            // style={{ border: "1px solid lightgrey" }}
           >
             NextJS
           </span>
           <span
             className="about__section-skills-skill"
-            style={{ border: "1px solid lightgrey" }}
+            // style={{ border: "1px solid lightgrey" }}
           >
             Express
           </span>
           <span
             className="about__section-skills-skill"
-            style={{ border: "1px solid #46a93a" }}
+            // style={{ border: "1px solid #46a93a" }}
           >
             MongoDB
           </span>
           <span
             className="about__section-skills-skill"
-            style={{ border: "1px solid #ac4343" }}
+            // style={{ border: "1px solid #ac4343" }}
           >
             Mongoose
           </span>
           <span
             className="about__section-skills-skill"
-            style={{ border: "1px solid #31638d " }}
+            // style={{ border: "1px solid #31638d " }}
           >
             PostgreSQL
           </span>
           <span
             className="about__section-skills-skill"
-            style={{ border: "1px solid #2aa474" }}
+            // style={{ border: "1px solid #2aa474" }}
           >
             Django
           </span>
           <span
             className="about__section-skills-skill"
-            style={{ border: "1px solid lightgrey" }}
+            // style={{ border: "1px solid lightgrey" }}
           >
             Git Version Control
           </span>
           <span
             className="about__section-skills-skill"
-            style={{ border: "1px solid lightgrey" }}
+            // style={{ border: "1px solid lightgrey" }}
           >
             Github
           </span>
