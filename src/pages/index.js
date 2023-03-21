e77bf836d6be5790e3b02517667a5fce7ca3d0d8
cps97 <colinstoutt@gmail.com>
2023-03-20 19:57:55 -0700

    update index for favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,13 @@
+import Head from "next/head";
+
 export default function Index() {
   return (
     <>
+      <Head>
+        <title>Colin Stoutt | Web Developer</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <main className="index">
         <h1 className="index__name fade-in-image">colinStoutt</h1>
         <h1 className="index__title fade-in-image">webDeveloper</h1>
