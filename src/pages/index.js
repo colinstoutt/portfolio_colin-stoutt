01b9cedcb02e30b315a60ff43b2277f371b47f0f
cps97 <colinstoutt@gmail.com>
2023-04-18 15:30:08 -0700

    fix file name for vercel

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Contact from "@/components/Contact";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
-import About from "@/components/about";
+import About from "@/components/About";
 import Head from "next/head";
 
 export default function Index() {
