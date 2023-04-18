8da6eaaf59dfd656f76009a15b03120b341bb846
cps97 <colinstoutt@gmail.com>
2023-04-18 15:29:03 -0700

    add &apos;

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
       </h1>
       <p>
         Have a question or suggestion? Click the link below or message me on
-        LinkedIn and I'll get back to you as soon as possible!
+        LinkedIn and I&apos;ll get back to you as soon as possible!
       </p>
       <a href="mailto: colin.stoutt22@gmail.com" className="contact__btn">
         <EmailRoundedIcon sx={{ color: "white", fontSize: "1rem" }} />
