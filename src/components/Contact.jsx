3b09d04829293910f928d8dada60f39e0ecef8f8
cps97 <colinstoutt@gmail.com>
2023-04-19 20:06:46 -0700

    add github and linkedin btns

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import EmailRoundedIcon from "@mui/icons-material/EmailRounded";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const Contact = () => {
   return (
@@ -11,10 +13,23 @@ const Contact = () => {
         Have a question or suggestion? Click the link below or message me on
         LinkedIn and I&apos;ll get back to you as soon as possible!
       </p>
-      <a href="mailto: colin.stoutt22@gmail.com" className="contact__btn">
-        <EmailRoundedIcon sx={{ color: "white", fontSize: "1rem" }} />
-        <span>Email</span>
-      </a>
+      <div className="contact__btns">
+        <a href="mailto: colin.stoutt22@gmail.com" className="contact__btn">
+          <EmailRoundedIcon sx={{ color: "lightgrey", fontSize: "1rem" }} />
+          <span>Email</span>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/colin-stoutt/"
+          className="contact__btn"
+        >
+          <LinkedInIcon sx={{ color: "lightgrey", fontSize: "1rem" }} />
+          <span>LinkedIn</span>
+        </a>
+        <a href="https://github.com/colinstoutt" className="contact__btn">
+          <GitHubIcon sx={{ color: "lightgrey", fontSize: "1rem" }} />
+          <span>GitHub</span>
+        </a>
+      </div>
     </main>
   );
 };
