75770d630add4bb60373bfcb60cb55412f4e966a
cps97 <colinstoutt@gmail.com>
2023-04-19 20:07:37 -0700

    add image, change icon size

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,25 +6,30 @@ const Hero = () => {
   return (
     <>
       <main className="index">
-        <div className="index__text">
-          <h1 className="index__name fade-in-image">colinStoutt</h1>
-          <h1 className="index__title fade-in-image">softwareDeveloper</h1>
+        <div className="index__hero">
+          <img src="https://i.imgur.com/neM62rY.jpg" alt="colin stoutt" />
+
+          <div className="index__text">
+            <h1 className="index__name">colinStoutt</h1>
+            <h1 className="index__title">softwareDeveloper</h1>
+          </div>
         </div>
         <Link
           to="about"
-          className="nav__link fade-in-image"
+          className="nav__link"
           smooth={true}
-          offset={-30}
+          offset={-80}
           duration={500}
         >
           <ExpandMoreRoundedIcon
+            className="mobile"
             sx={{
               position: "absolute",
-              bottom: "0",
+              bottom: "1.5rem",
               left: "50%",
               transform: "translate(-50%, -50%)",
               color: "lightgrey",
-              fontSize: "3rem",
+              fontSize: "4rem",
             }}
           />
         </Link>
